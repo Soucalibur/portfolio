c4a8c7424d36c3ac40379c377109634ba68fc182
Matthew Gallar <matthewgallar2112@outlook.com>
2023-01-23 16:51:17 -0300

    Styled Navbar

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -4,19 +4,20 @@ import s from "./navbar.css"
 const  Navbar =()=>{
     return(
         <div className="backgroundNav">
-            
-            <Link to="/home">
-                Home
-            </Link>
-            <Link to="/skills">
-                Skills
-            </Link>
-            <Link to="/projects">
-                Projects
-            </Link>
-            <Link to="/contactme">
-                ContactMe
-            </Link>
+            <div className="containerButtonsNav">
+                <Link to="/home">
+                        Home
+                </Link>
+                <Link to="/skills">
+                        Skills    
+                </Link>
+                <Link to="/projects"> 
+                        Projects
+                </Link>
+                <Link to="/contactme">
+                        ContactMe
+                </Link>
+            </div>
         </div>
     )
 }
