b92ec66c903345458a84adf94c1e52aa7b68ff50
Matthew Gallar <matthewgallar2112@outlook.com>
2023-04-25 17:55:12 -0300

    adding loading

--- client/src/components/helper/imgProyects/imagenesProjects.js
+++ client/src/components/helper/imgProyects/imagenesProjects.js
@@ -1,7 +1,8 @@
 
 const ImagenesProjects = {
     proyecto1HenryCountries: require("./HenryCountries.png"),
-    proyecto2LatamCom: require("./LatamCom.png")
+    proyecto2LatamCom: require("./LatamCom.png"),
+    loading1: require("../imgBackgrounds/loading1.gif")
 
 }
 
--- client/src/components/index/landingPage.js
+++ client/src/components/index/landingPage.js
@@ -3,18 +3,31 @@ import AboutMe from "../aboutMe/AboutMe"
 import Skills from "../skills/Skills"
 import Projects from "../projects/Projects"
 import ContactMe from "../contactMe/ContactMe"
+import ImagenesProjects from "../helper/imgProyects/imagenesProjects"
 
 import s from "./landingPage.css"
 
 const Index = ()=>{
+    window.onload = ()=>{
+        const primerDiv = document.getElementById("primerDiv")
+        primerDiv.style.display = "none"
+        const segundoDiv = document.getElementById("segundoDiv")
+        segundoDiv.hidden = false
+        segundoDiv.className = "containerIndex"
+    }
 
     return(
         <div>
-            <Navbar></Navbar>
-            <AboutMe></AboutMe>
-            <Skills></Skills>
-            <Projects></Projects>
-            <ContactMe></ContactMe>
+            <div id="primerDiv">
+                <img src={ImagenesProjects.loading1} alt="logoContactPoint" className="imgLoading"></img>
+            </div>
+            <div id="segundoDiv" hidden={true}>
+                <Navbar></Navbar>
+                <AboutMe></AboutMe>
+                <Skills></Skills>
+                <Projects></Projects>
+                <ContactMe></ContactMe>
+            </div>
         </div>
     )
 }
